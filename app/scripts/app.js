019dd99e75357c7f2d20c3c9aa68cb9b62459c62
Alex Jarvis <alxjrvs@gmail.com>
2014-11-20 20:12:49 -0500

    Initial

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name annotubeApp
+ * @description
+ * # annotubeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('annotubeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name annotubeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the annotubeApp
+ */
+angular.module('annotubeApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
